4f0ce08f24a4e7defdb63dec4b2570fa461c9c8b
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-04-07 18:32:30 +0500

    wip

--- lib/helpers/DBHelper.dart
+++ lib/helpers/DBHelper.dart
@@ -1,21 +1,31 @@
-import 'package:sqflite/sqflite.dart' as sql;
+import 'dart:io';
+import 'package:hive/hive.dart';
 
-import 'package:path/path.dart' as path;
+// @HiveType(typeId: 1)
+// class Todo {
+//   Todo({required this.name, required this.age, required this.friends});
+
+//   @HiveField(0)
+//   String id;
+
+//   @HiveField(1)
+//   int age;
+
+//   @HiveField(2)
+//   List<String> friends;
+// }
 
 class DBHelper {
   static const _DB_TABLE = 'todos';
 
-  static Future<sql.Database> database() async {
-    final dbPath = await sql.getDatabasesPath();
-    return sql.openDatabase(
-      path.join(dbPath, '$_DB_TABLE.db'),
-      onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE $_DB_TABLE(id TEXT PRIMARY KEY, title TEXT, description TEXT, streak INTEGER)',
-        );
-      },
-      version: 1,
-    );
+  static Future database() async {
+    var path = Directory.current.path;
+    Hive
+      ..init(path)
+      ..registerAdapter(PersonAdapter());
+
+    var box = await Hive.openBox(_DB_TABLE);
+    return box;
   }
 
   static Future<void> insert(String table, Map<String, Object> data) async {
